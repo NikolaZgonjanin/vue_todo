d04f1979ca8159b718918fca131510e717040f0c
Nikola Zgonjanin <zgonjanin.nikola.92@gmail.com>
2024-01-12 21:56:09 +0100

    Everything :)

---
+++ vue-todos/src/components/TodoButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <button>
+    <slot>Click me!</slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+</style>
---
+++ vue-todos/src/components/TodoHeader.vue
@@ -0,0 +1,59 @@
+<script setup>
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav class="container">
+        <div class="branding">
+          <img src="../assets/Vue.js_Logo_2.svg" alt="app image">
+          <h1>Vue Todo List</h1>
+        </div>
+        <ul class="nav-routes">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+        white-space: nowrap;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
